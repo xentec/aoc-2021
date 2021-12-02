95e2d053c4a19e09ff4350e223dcc6f3299637c6
Andrej <xentec@aix0.eu>
2021-12-02 10:39:47 +0100

    02: dive instructions

---
+++ 02-dive/main.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+const Command = struct {
+    const Type = enum {
+        forward,
+        up,
+        down,
+    };
+
+    type: Type,
+    value: i32,
+};
+
+
+pub fn main() !void {
+    var stdin = std.io.bufferedReader(std.io.getStdIn().reader());
+    var stream = stdin.reader();
+  
+    var depth: i32 = 0;
+    var position: i32 = 0;
+
+    var buf: [32]u8 = undefined;
+    while(try stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        var splitter = std.mem.split(u8, line, " ");
+
+        var cmd = Command {
+            .type = std.meta.stringToEnum(Command.Type, splitter.next().?).?,
+            .value = try std.fmt.parseInt(i32, splitter.next().?, 10),
+        };
+
+        switch(cmd.type) {
+            .down => depth += cmd.value,
+            .up => depth -= cmd.value,
+            .forward => position += cmd.value,
+        }
+
+    }
+    std.debug.print("dive position: {}, depth: {}; ({})\n", .{position, depth, position*depth});
+}
