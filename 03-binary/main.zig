d1d1f2bbcd33f753187c43eca8a00913efe47b3b
Xentec <xentec@aix0.eu>
2021-12-04 02:38:12 +0100

    03: binary diagnosis approximation

--- 03-binary/main.zig
+++ 03-binary/main.zig
@@ -1,24 +1,48 @@
 const std = @import("std");
 
+fn findRating(numbers: []u12, most: bool) u12 {
+    const NumType = @TypeOf(numbers[0]);
+
+    std.sort.sort(NumType, numbers, {}, comptime std.sort.desc(NumType));
+    var shift: u4 = @bitSizeOf(NumType) - 1;
+    var nums = numbers;
+    while (nums.len > 1) {
+        var idx = nums.len / 2;
+        const shiftedBit = @as(NumType, 1) << shift;
+        shift -= 1;
+        const middleBit = nums[idx] & shiftedBit;
+        const incr = if (middleBit > 0) @as(isize, 1) else @as(isize, -1);
+        if (nums.len > 2) {
+            while (nums[idx] & shiftedBit == middleBit) // find 1->0 change offset
+                idx = @intCast(usize, @intCast(isize, idx) + incr);
+
+            if (incr == -1)
+                idx += 1;
+        }
+        const ones = nums[0..idx];
+        const zeroes = nums[idx..];
+        const takeOnes = if (ones.len == zeroes.len) most else most and ones.len > zeroes.len or !most and ones.len < zeroes.len;
+        nums = if (takeOnes) ones else zeroes;
+    }
+    return nums[0];
+}
+
 pub fn main() !void {
     var stdin = std.io.bufferedReader(std.io.getStdIn().reader());
     var stream = stdin.reader();
-  
-    var counter = std.mem.zeroes([12]i32);
 
-    var buf: [32]u8 = undefined;
-    while(try stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        for (line) |bit, idx|
-            counter[idx] += if (bit == '1') @as(i32, 1) else @as(i32, -1);
+    const NumType = u12;
+    var numbers = try std.ArrayList(NumType).initCapacity(std.heap.page_allocator, 1024);
+    defer numbers.deinit();
+
+    var buf: [16]u8 = undefined;
+    while (try stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        const num = try std.fmt.parseUnsigned(NumType, line, 2);
+        try numbers.append(num);
     }
 
-    var gamma: u12 = 0;
-    for (counter) |bitCount, idx| {
-        const bit: u12 = @boolToInt(bitCount > 0);
-        gamma |= bit <<| @intCast(u12, counter.len - idx - 1);
-    }
-    var epsilon: u12 = ~gamma;
-    var powerConsumption = @as(u32, gamma) * epsilon;
+    const o2Rating = findRating(numbers.items, true);
+    const co2Rating = findRating(numbers.items, false);
 
-    std.debug.print("power consumption: {}\n", .{powerConsumption});
+    std.debug.print("O2 rating: {}, CO2 rating: {} ({})\n", .{ o2Rating, co2Rating, @as(u32, o2Rating) * co2Rating });
 }
