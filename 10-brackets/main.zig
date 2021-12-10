af85e776e78c638dc8d27573a7b012a42881766d
Andrej <xentec@aix0.eu>
2021-12-10 20:01:49 +0100

    10: stacking brackets to completion

--- 10-brackets/main.zig
+++ 10-brackets/main.zig
@@ -10,10 +10,11 @@ pub fn main() !void {
     defer arena.deinit();
     var alloc = arena.allocator();
 
-    var stack = try std.ArrayList(u8).initCapacity(alloc, 100);
+    var stack = try std.ArrayList(u8).initCapacity(alloc, 32);
     defer stack.deinit();
+    var score_list = try std.ArrayList(usize).initCapacity(alloc, 32);
 
-    var output: usize = 0;
+lines:
     while (try stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         stack.clearRetainingCapacity();
         for (line) |char| {
@@ -26,20 +27,30 @@ pub fn main() !void {
                     const matching = char - @as(u8, if (char==')') 1 else 2);
                     const match = if(stack.popOrNull()) |c| c == matching else false;
                     if(!match) {
-                        output += @as(usize, switch(char) {
-                            ')' => 3,
-                            ']' => 57,
-                            '}' => 1197,
-                            '>' => 25137,
-                            else => unreachable,
-                        });
-                        break;
+                        continue :lines;
                     }
                 },
                 else => unreachable,
             }
         }
+
+        var score: usize = 0;
+        for (stack.items) |_, i| {
+            const c = stack.items[stack.items.len-i-1];
+            score *= 5;
+            score += @as(usize, switch(c) {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                else => unreachable,
+            });
+        }
+        if (score > 0)
+            try score_list.append(score);
     }
 
+    std.sort.sort(usize, score_list.items, {}, comptime std.sort.asc(usize));
+    var output = score_list.items[score_list.items.len/2];
     pr("{}\n", .{output});
 }
