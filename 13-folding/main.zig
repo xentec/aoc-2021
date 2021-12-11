d2ab371458002e3222c9fae519b4ff3c8b863775
Andrej <xentec@aix0.eu>
2021-12-11 06:50:45 +0100

    13: folding manuals

---
+++ 13-folding/main.zig
@@ -0,0 +1,68 @@
+const std = @import("std");
+const pr = std.debug.print;
+
+const Point = struct {
+    const Type = i16;
+    const Self = @This();
+
+    x: Type,
+    y: Type,
+
+    fn parse(str: []const u8) !Self {
+        var tok = std.mem.tokenize(u8, str, ",");
+        return Self {
+            .x = try std.fmt.parseInt(Type, tok.next() orelse return error.BadInput, 10),
+            .y = try std.fmt.parseInt(Type, tok.next() orelse return error.BadInput, 10),
+        };
+    }
+};
+
+pub fn main() !void {
+    var stdin = std.io.bufferedReader(std.io.getStdIn().reader());
+    var input = stdin.reader();
+    var buf: [128]u8 = undefined;
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    var alloc = arena.allocator();
+
+    var paper = std.AutoArrayHashMap(Point, void).init(alloc);
+
+    while (try input.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        if (line.len == 0)
+            break;
+
+        try paper.put(try Point.parse(line), {});
+    }
+    while (try input.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        const eq_pos = std.mem.indexOfPosLinear(u8, line, 0, "=").?;
+        const offset = try std.fmt.parseInt(i16, line[eq_pos+1..], 10);
+        var tmp = std.ArrayList(Point).init(alloc);
+        switch(line[eq_pos-1]) {
+            'x' => {
+                for (paper.keys()) |p| {
+                    if (p.x > offset)
+                        try tmp.append(p);
+                }
+                for (tmp.items) |p| {
+                    _ = paper.swapRemove(p);
+                    try paper.put(.{.x=offset-(p.x-offset), .y=p.y}, {});
+                }
+            },
+            'y' => {
+                for (paper.keys()) |p| {
+                    if (p.y > offset)
+                        try tmp.append(p);
+                }
+                for (tmp.items) |p| {
+                    _ = paper.swapRemove(p);
+                    try paper.put(.{.y=offset-(p.y-offset), .x=p.x}, {});
+                }
+            },
+            else => unreachable,
+        }
+        break;
+    }
+    var output: usize = paper.count();
+    std.fmt.format(std.io.getStdOut().writer(), "{}\n", .{output}) catch unreachable;
+}
